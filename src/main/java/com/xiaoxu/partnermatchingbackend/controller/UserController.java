4070d55771ec68e13eccce09cfb0fc189458b1fc
xiaoxu954 <x17605049954@163.com>
2023-11-28 20:48:22 +0800

    编辑距离算法及测试工具类、实现匹配功能

--- src/main/java/com/xiaoxu/partnermatchingbackend/controller/UserController.java
+++ src/main/java/com/xiaoxu/partnermatchingbackend/controller/UserController.java
@@ -189,6 +189,21 @@ public class UserController {
         return ResultUtils.success(userPage);
     }
 
+    /**
+     * 获取最匹配的用户
+     *
+     * @param num
+     * @param request
+     * @return
+     */
+    @GetMapping("/match")
+    public BaseResponse<List<User>> matchUsers(long num, HttpServletRequest request) {
+        if (num <= 0 || num > 20) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = userService.getLoginUser(request);
+        return ResultUtils.success(userService.matchUsers(num, user));
+    }
 
 }
 
